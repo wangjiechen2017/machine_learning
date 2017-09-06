c55a3775a9df7c63b5c50cfe57a212026cb3acda
wangjiechen <wang_jiechen@163.com>
2017-09-06 23:51:53 +0800

    knn p19

--- KNN/knn.py
+++ KNN/knn.py
@@ -1,1 +1,21 @@
-i
+from numpy import *
+import operator
+
+def createDateSet():
+    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    labels = ['A','A','B','B']
+    return group, labels
+    
+    
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]                              #得到行数
+    diffMat = tile(inX, tile(dataSetSize,1)) - dataSetSize      #tile函数,重复array
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis = 1)                       #sum(axis = 1)行方向上相加
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    
+    
+    
+    
